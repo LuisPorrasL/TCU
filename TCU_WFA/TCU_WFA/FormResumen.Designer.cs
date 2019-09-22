4adf9eac07bbd4b58ebcd80234bbe8bc088face5
Ariel Arias Rojas <ariel.ariasrojas@ucr.ac.cr>
2019-09-22 16:54:34 -0600

    Primeros avances en la creación del excel

--- TCU_WFA/TCU_WFA/FormResumen.Designer.cs
+++ TCU_WFA/TCU_WFA/FormResumen.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.botonVolver = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.botonGenerarInformeExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // botonVolver
@@ -41,18 +44,42 @@
             this.botonVolver.UseVisualStyleBackColor = true;
             this.botonVolver.Click += new System.EventHandler(this.botonVolver_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 292);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // botonGenerarInformeExcel
+            // 
+            this.botonGenerarInformeExcel.Location = new System.Drawing.Point(13, 312);
+            this.botonGenerarInformeExcel.Name = "botonGenerarInformeExcel";
+            this.botonGenerarInformeExcel.Size = new System.Drawing.Size(118, 23);
+            this.botonGenerarInformeExcel.TabIndex = 2;
+            this.botonGenerarInformeExcel.Text = "Generar Excel";
+            this.botonGenerarInformeExcel.UseVisualStyleBackColor = true;
+            this.botonGenerarInformeExcel.Click += new System.EventHandler(this.botonGenerarInformeExcel_Click);
+            // 
             // FormResumen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botonGenerarInformeExcel);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.botonVolver);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FormResumen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Resumen";
             this.Load += new System.EventHandler(this.FormResumen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Button botonVolver;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button botonGenerarInformeExcel;
     }
 }
--- TCU_WFA/TCU_WFA/FormResumen.cs
+++ TCU_WFA/TCU_WFA/FormResumen.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OfficeOpenXml;
+using System.IO;
+using System.Diagnostics;
 
 namespace TCU_WFA
 {
@@ -21,6 +24,69 @@ namespace TCU_WFA
         private void FormResumen_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Método utilizado para generar el excel al presionar el botón
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void botonGenerarInformeExcel_Click(object sender, EventArgs e)
+        {
+            //Se genera el excel con toda la información correspondiente del resumen
+            GenerarExcelResumen();
+        }
+
+        /// <summary>
+        /// Método para generar el documento de Excel con el resumen y los datos correspondientes.
+        /// </summary>
+        private void GenerarExcelResumen()
+        {
+            //Se obtienen los datos a utilizar en el resumen
+            //ToDo
+
+            //Se genera el documento excel
+            CrearDocumentoExcel();
+        }
+
+        private void CrearDocumentoExcel()
+        {
+            //Se crea una instancia del paquete de excel del documento a utilizar
+            ExcelPackage documentoExcel = new ExcelPackage();
+
+            //Se crea la hoja que se va a generar
+            ExcelWorksheet hojaResumen = documentoExcel.Workbook.Worksheets.Add("Resumen");
+
+            //Se establece el primer rango de celdas a utilizar para la información general
+            ExcelRange celdasInformacionGeneral = hojaResumen.Cells[1, 1, 8, 3];
+
+            //Se completan las celdas con sus valores respectivos
+            celdasInformacionGeneral[1, 1].Value = "Fecha referencia";
+            celdasInformacionGeneral[1, 3].Value = DateTime.Now.ToString();
+
+            //Se guarda el documento
+            string ubicacionDocumentos = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string nombreDocumentoResumen = ubicacionDocumentos + @"\" + "Resumen_" + DateTime.Now.ToString().Replace("/",".").Replace(":", ".").Replace(" ","_").Replace("\\", ".") + ".xlsx";
+
+            try
+            {
+                documentoExcel.SaveAs(new FileInfo(nombreDocumentoResumen));
+
+                //Se muestra el mensaje que indica al usuario en donde quedó el documento
+                MessageBox.Show("El documento se guardó con exito en: " + nombreDocumentoResumen);
+            }
+            catch
+            {
+                //Se muestra el mensaje que indica al usuario en donde quedó el documento
+                MessageBox.Show("Ocurrió un error al guardar el documento","Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+
+            
+
+
+            //Se cierra el documento
+            documentoExcel.Dispose();
         }
     }
 }
