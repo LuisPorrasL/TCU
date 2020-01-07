6d2c9738899ec8bf3f93684b05f310db70f088f5
Luis Porras <luis.porrasledezma@ucr.ac.cr>
2020-01-07 13:17:01 -0600

    Se crea la clase para administrar la cofig del programa (alertas y preferencias de despliegue)

---
+++ TCU_WFA/TCU_WFA/ProgramConfiguration.cs
@@ -0,0 +1,13 @@
+﻿// Hecho por Luis Porras L.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCU_WFA
+{
+    class ProgramConfiguration
+    {
+    }
+}
