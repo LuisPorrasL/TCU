1946b9b2af8d09d8416b6b50cf919987b6633125
Ariel Arias <ariel.ariasrojas@ucr.ac.cr>
2020-02-11 13:01:41 -0600

    Se agrega la información

--- TCU_WFA/TCU_WFA/FormInformacion.Designer.cs
+++ TCU_WFA/TCU_WFA/FormInformacion.Designer.cs
@@ -29,6 +29,20 @@
         private void InitializeComponent()
         {
             this.botonVolver = new System.Windows.Forms.Button();
+            this.groupBoxInformacion = new System.Windows.Forms.GroupBox();
+            this.labelZootecnico = new System.Windows.Forms.Label();
+            this.labelProgramadores = new System.Windows.Forms.Label();
+            this.labelEstudiante4 = new System.Windows.Forms.Label();
+            this.labelEstudiante3 = new System.Windows.Forms.Label();
+            this.labelEstudiante2 = new System.Windows.Forms.Label();
+            this.labelEstudiante1 = new System.Windows.Forms.Label();
+            this.labelEstudiantes = new System.Windows.Forms.Label();
+            this.labelNombreProfesor = new System.Windows.Forms.Label();
+            this.labelProfesor = new System.Windows.Forms.Label();
+            this.labelNombreTCU = new System.Windows.Forms.Label();
+            this.labelTCU = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.groupBoxInformacion.SuspendLayout();
             this.SuspendLayout();
             // 
             // botonVolver
@@ -41,18 +55,161 @@
             this.botonVolver.UseVisualStyleBackColor = true;
             this.botonVolver.Click += new System.EventHandler(this.botonVolver_Click);
             // 
+            // groupBoxInformacion
+            // 
+            this.groupBoxInformacion.Controls.Add(this.labelZootecnico);
+            this.groupBoxInformacion.Controls.Add(this.labelProgramadores);
+            this.groupBoxInformacion.Controls.Add(this.labelEstudiante4);
+            this.groupBoxInformacion.Controls.Add(this.labelEstudiante3);
+            this.groupBoxInformacion.Controls.Add(this.labelEstudiante2);
+            this.groupBoxInformacion.Controls.Add(this.labelEstudiante1);
+            this.groupBoxInformacion.Controls.Add(this.labelEstudiantes);
+            this.groupBoxInformacion.Controls.Add(this.labelNombreProfesor);
+            this.groupBoxInformacion.Controls.Add(this.labelProfesor);
+            this.groupBoxInformacion.Controls.Add(this.labelNombreTCU);
+            this.groupBoxInformacion.Controls.Add(this.labelTCU);
+            this.groupBoxInformacion.Controls.Add(this.labelInfo);
+            this.groupBoxInformacion.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxInformacion.Name = "groupBoxInformacion";
+            this.groupBoxInformacion.Size = new System.Drawing.Size(776, 397);
+            this.groupBoxInformacion.TabIndex = 4;
+            this.groupBoxInformacion.TabStop = false;
+            // 
+            // labelZootecnico
+            // 
+            this.labelZootecnico.AutoSize = true;
+            this.labelZootecnico.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.labelZootecnico.Location = new System.Drawing.Point(51, 293);
+            this.labelZootecnico.Name = "labelZootecnico";
+            this.labelZootecnico.Size = new System.Drawing.Size(89, 16);
+            this.labelZootecnico.TabIndex = 11;
+            this.labelZootecnico.Text = "Zootécnico:";
+            // 
+            // labelProgramadores
+            // 
+            this.labelProgramadores.AutoSize = true;
+            this.labelProgramadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.labelProgramadores.Location = new System.Drawing.Point(51, 194);
+            this.labelProgramadores.Name = "labelProgramadores";
+            this.labelProgramadores.Size = new System.Drawing.Size(120, 16);
+            this.labelProgramadores.TabIndex = 10;
+            this.labelProgramadores.Text = "Programadores:";
+            // 
+            // labelEstudiante4
+            // 
+            this.labelEstudiante4.AutoSize = true;
+            this.labelEstudiante4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelEstudiante4.Location = new System.Drawing.Point(182, 293);
+            this.labelEstudiante4.Name = "labelEstudiante4";
+            this.labelEstudiante4.Size = new System.Drawing.Size(45, 16);
+            this.labelEstudiante4.TabIndex = 9;
+            this.labelEstudiante4.Text = "label4";
+            // 
+            // labelEstudiante3
+            // 
+            this.labelEstudiante3.AutoSize = true;
+            this.labelEstudiante3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelEstudiante3.Location = new System.Drawing.Point(182, 252);
+            this.labelEstudiante3.Name = "labelEstudiante3";
+            this.labelEstudiante3.Size = new System.Drawing.Size(45, 16);
+            this.labelEstudiante3.TabIndex = 8;
+            this.labelEstudiante3.Text = "label3";
+            // 
+            // labelEstudiante2
+            // 
+            this.labelEstudiante2.AutoSize = true;
+            this.labelEstudiante2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelEstudiante2.Location = new System.Drawing.Point(182, 223);
+            this.labelEstudiante2.Name = "labelEstudiante2";
+            this.labelEstudiante2.Size = new System.Drawing.Size(45, 16);
+            this.labelEstudiante2.TabIndex = 7;
+            this.labelEstudiante2.Text = "label2";
+            // 
+            // labelEstudiante1
+            // 
+            this.labelEstudiante1.AutoSize = true;
+            this.labelEstudiante1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelEstudiante1.Location = new System.Drawing.Point(182, 194);
+            this.labelEstudiante1.Name = "labelEstudiante1";
+            this.labelEstudiante1.Size = new System.Drawing.Size(45, 16);
+            this.labelEstudiante1.TabIndex = 6;
+            this.labelEstudiante1.Text = "label1";
+            // 
+            // labelEstudiantes
+            // 
+            this.labelEstudiantes.AutoSize = true;
+            this.labelEstudiantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.labelEstudiantes.Location = new System.Drawing.Point(19, 161);
+            this.labelEstudiantes.Name = "labelEstudiantes";
+            this.labelEstudiantes.Size = new System.Drawing.Size(309, 16);
+            this.labelEstudiantes.TabIndex = 5;
+            this.labelEstudiantes.Text = "Estudiantes que participaron en el proyecto";
+            // 
+            // labelNombreProfesor
+            // 
+            this.labelNombreProfesor.AutoSize = true;
+            this.labelNombreProfesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelNombreProfesor.Location = new System.Drawing.Point(184, 107);
+            this.labelNombreProfesor.Name = "labelNombreProfesor";
+            this.labelNombreProfesor.Size = new System.Drawing.Size(59, 16);
+            this.labelNombreProfesor.TabIndex = 4;
+            this.labelNombreProfesor.Text = "Profesor";
+            // 
+            // labelProfesor
+            // 
+            this.labelProfesor.AutoSize = true;
+            this.labelProfesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.labelProfesor.Location = new System.Drawing.Point(19, 107);
+            this.labelProfesor.Name = "labelProfesor";
+            this.labelProfesor.Size = new System.Drawing.Size(160, 16);
+            this.labelProfesor.TabIndex = 3;
+            this.labelProfesor.Text = "Profesor y supervisor:";
+            // 
+            // labelNombreTCU
+            // 
+            this.labelNombreTCU.AutoSize = true;
+            this.labelNombreTCU.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.labelNombreTCU.Location = new System.Drawing.Point(182, 78);
+            this.labelNombreTCU.Name = "labelNombreTCU";
+            this.labelNombreTCU.Size = new System.Drawing.Size(36, 16);
+            this.labelNombreTCU.TabIndex = 2;
+            this.labelNombreTCU.Text = "TCU";
+            // 
+            // labelTCU
+            // 
+            this.labelTCU.AutoSize = true;
+            this.labelTCU.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTCU.Location = new System.Drawing.Point(19, 78);
+            this.labelTCU.Name = "labelTCU";
+            this.labelTCU.Size = new System.Drawing.Size(128, 16);
+            this.labelTCU.TabIndex = 1;
+            this.labelTCU.Text = "Nombre del TCU:";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(19, 29);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(80, 16);
+            this.labelInfo.TabIndex = 0;
+            this.labelInfo.Text = "Descripción";
+            // 
             // FormInformacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxInformacion);
             this.Controls.Add(this.botonVolver);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FormInformacion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormInformacion";
+            this.Text = "Información de la aplicación";
             this.Load += new System.EventHandler(this.FormInformacion_Load);
+            this.groupBoxInformacion.ResumeLayout(false);
+            this.groupBoxInformacion.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +217,18 @@
         #endregion
 
         private System.Windows.Forms.Button botonVolver;
+        private System.Windows.Forms.GroupBox groupBoxInformacion;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelNombreTCU;
+        private System.Windows.Forms.Label labelTCU;
+        private System.Windows.Forms.Label labelNombreProfesor;
+        private System.Windows.Forms.Label labelProfesor;
+        private System.Windows.Forms.Label labelEstudiante4;
+        private System.Windows.Forms.Label labelEstudiante3;
+        private System.Windows.Forms.Label labelEstudiante2;
+        private System.Windows.Forms.Label labelEstudiante1;
+        private System.Windows.Forms.Label labelEstudiantes;
+        private System.Windows.Forms.Label labelProgramadores;
+        private System.Windows.Forms.Label labelZootecnico;
     }
 }
--- TCU_WFA/TCU_WFA/FormInformacion.cs
+++ TCU_WFA/TCU_WFA/FormInformacion.cs
@@ -12,6 +12,18 @@ namespace TCU_WFA
 {
     public partial class FormInformacion : DefaultForm
     {
+        //Descripción general TCU
+        private const string INFO_TCU = "Esta aplicación fue desarrollada como un proyecto del Trabajo Comunal Universitario de la Universidad de Costa Rica.";
+
+        //Nombre del TCU
+        private const string NOMBRE_TCU = "TC-228 Asesoría agroeconómica a organizaciones de productores agrícolas y agroindustriales";
+
+        //Nombre del profesor
+        private const string NOMBRE_PROFESOR = "Alberth Campos Arguello";
+
+        //Nombres
+        private string[] ESTUDIANTES = new string[4]{ "Ariel Stiven Arias Rojas", "José Alberto Soto Li", "Luis Andrés Porras Ledezma", "Leonel Rodolfo Sánchez Loaiza" };
+
         public FormInformacion()
         {
             InitializeComponent();
@@ -19,7 +31,13 @@ namespace TCU_WFA
 
         private void FormInformacion_Load(object sender, EventArgs e)
         {
-
+            labelInfo.Text = INFO_TCU;
+            labelNombreTCU.Text = NOMBRE_TCU;
+            labelNombreProfesor.Text = NOMBRE_PROFESOR;
+            labelEstudiante1.Text = ESTUDIANTES[0];
+            labelEstudiante2.Text = ESTUDIANTES[1];
+            labelEstudiante3.Text = ESTUDIANTES[2];
+            labelEstudiante4.Text = ESTUDIANTES[3];
         }
     }
 }
